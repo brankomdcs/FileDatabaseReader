fb0a794d5b60701f66e0ff6666876a87ba9e20e7
brankomdcs <brankom@microsoft.com>
2021-07-15 18:42:05 +0200

    Introducing more complexed, nested structure to tryout nlohmann serialization/deserialization in that case.

--- FileReader/DataDefinitions.h
+++ FileReader/DataDefinitions.h
@@ -10,23 +10,47 @@ namespace DataDefinition
 	struct Person
 	{
 		Person() {};
-		Person(string person_name, int person_age) :
+		Person(string person_name, int person_age, vector<Person> persons_friends = vector<Person>()) :
 			name(person_name),
-			age(person_age) {}
+			age(person_age),
+			friends(persons_friends)
+		{}
 
 		string name;
 		int age;
+		vector<Person> friends;
 	};
 
 	inline void to_json(json& j, const Person& r) {
-		j = json{
+		if (r.friends.empty()) {
+			j = json{
 			{"name", r.name},
 			{"age", r.age}
+			};
+			return;
+		}
+
+		json friendsJson;
+		for (Person person : r.friends)
+		{
+			json personJson;
+			to_json(personJson, person);
+			friendsJson.push_back(personJson);
+		}
+		j = json{
+			{"name", r.name},
+			{"age", r.age},
+			{"friends", friendsJson}
 		};
 	}
 
 	inline void from_json(const json& j, Person& r) {
-		r.name = j.at("name").get<std::string>();
+		r.name = j.at("name").get<string>();
 		r.age = j.at("age").get<int>();
+
+		if (j.find("friends") == j.end())
+			r.friends = vector<Person>();
+		else
+			r.friends = j.at("friends").get_to<vector<Person>>(r.friends);
 	}
 }
--- FileReader/FileReader.cpp
+++ FileReader/FileReader.cpp
@@ -13,16 +13,19 @@ using namespace FileReader;
 int main()
 {
     FileDatabase* fileDatabase = FileDatabase::GetInstance();
+    vector<Person> everyonesFriends;
+    everyonesFriends.push_back(Person("Mile", 55));
+    everyonesFriends.push_back(Person("Rile", 45));
 
     vector<Person> people;
-    people.push_back(Person("Branko", 31));
-    people.push_back(Person("Marko", 32));
-    people.push_back(Person("Zarko", 30));
+    people.push_back(Person("Branko", 31, everyonesFriends));
+    people.push_back(Person("Marko", 32, everyonesFriends));
+    people.push_back(Person("Zarko", 30, everyonesFriends));
 
     fileDatabase->Save(people);
 
     vector<Person> retrievedPeople = fileDatabase->Get();
 
     for (Person person : retrievedPeople)
-        cout << person.name << ":" << person.age << "\n";
+        cout << person.name << ":" << person.age << ":" << person.friends[0].name << "\n";
 }
